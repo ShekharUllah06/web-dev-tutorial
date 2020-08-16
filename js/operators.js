2f405fa847edf992ebba86e09c170247cf426d93
Shekhar Abdullah <aa1912275@gmail.com>
2020-08-16 12:13:43 +0600

    operators

---
+++ js/operators.js
@@ -0,0 +1,28 @@
+document.write("<h1>JS Operators</h1>")
+//Arithmetic Operators
+var number1=20;
+var number2=10;
+
+var sum=number1+number2;
+document.write("<P>Addition of "+number1+" and "+ number2 +" is " + sum + "</p>");
+
+//Subtraction
+var subtraction=number1-number2;
+document.write("<P>Subtraction of "+number1+" and "+ number2 +" is " + subtraction + "</p>");
+
+//Multiplication
+var multiplication=number1*number2;
+document.write("<P>Multiplication of "+number1+" and "+ number2 +" is " + multiplication + "</p>");
+
+//Divition
+var divition=number1/number2;
+document.write("<P>Divition of "+number1+" and "+ number2 +" is " + divition + "</p>");
+
+//Remainder
+var remainder=number1%number2;
+document.write("<P>Remainder of "+number1+" and "+ number2 +" is " + remainder + "</p>");
+
+//Increment
+number1++;
+document.write("<P>Number1 variable contains "+ number1 +"</p>");
+
